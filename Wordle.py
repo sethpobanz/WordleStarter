b4d1d31d2f25f5a2531254373b8d7546517cabd4
sethpobanz <sethpobanz@gmail.com>
2024-01-10 13:59:16 -0700

    milestone1

--- Wordle.py
+++ Wordle.py
@@ -1,12 +1,9 @@
-# File: Wordle.py
-
 """
 This module is the starter file for the Wordle assignment.
 BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
 """
 
 import random
-
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
@@ -18,7 +15,14 @@ def wordle():
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
 
-# Startup code
+    # Milestone #1: Display a random word in the first row
+    random_word = random.choice(FIVE_LETTER_WORDS)
+    
+    # Display the random word in the first row
+    for col in range(N_COLS):
+        gw.set_square_letter(0, col, random_word[col])
 
+# Startup code
 if __name__ == "__main__":
     wordle()
+
